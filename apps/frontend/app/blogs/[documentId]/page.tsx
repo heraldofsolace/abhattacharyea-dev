1ad5e19dcf33ed4375b348d82a8162a035bbbca8
Your Name <you@example.com>
2025-06-28 19:01:48 +0530

    Fix images

--- apps/frontend/app/blogs/[documentId]/page.tsx
+++ apps/frontend/app/blogs/[documentId]/page.tsx
@@ -19,8 +19,8 @@ export default async function Blog({
         <main className="mt-20 p-10">
             <h1 className="text-9xl font-mono font-semibold">{blog.data.title}</h1>
             <figure className="mt-5 mb-3">
-              
-              <Image src={`${strapiURL}${blog.data.feature_image?.url}`} width={1000} height={300} alt="" />
+
+              <Image src={`${strapiURL}/uploads${blog.data.feature_image?.url}`} width={1000} height={300} alt="" />
               <figcaption></figcaption>
             </figure>
             <div className="mt-3">
--- apps/frontend/app/blogs/page.tsx
+++ apps/frontend/app/blogs/page.tsx
@@ -27,7 +27,7 @@ function BlogCard({post}:{post: APIResponseData<"api::post.post">}) {
                 </CardAction>
             </CardHeader>
             <CardContent className="flex flex-col justify-center">
-                <Image src={`${strapiURL}${post.feature_image?.url}`} className="mx-auto" width={500} height={300} alt="" />
+                <Image src={`${strapiURL}/uploads${post.feature_image?.url}`} className="mx-auto" width={500} height={300} alt="" />
                 <p className="mt-5">{post.summary}</p>
             </CardContent>
             <CardFooter className="absolute bottom-0 mb-3">
