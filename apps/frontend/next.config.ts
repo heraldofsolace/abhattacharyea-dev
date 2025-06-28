7eaba573fca49d27f21c19d0bed6896a3f2040d6
Your Name <you@example.com>
2025-06-28 18:30:03 +0530

    Fix images

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("http://localhost:1337/uploads/*")]
+    remotePatterns: [
+      new URL("http://localhost:1337/uploads/*"),
+      new URL("https://strapi-production-410d.up.railway.app/uploads/*")
+    ]
   }
 };
 
