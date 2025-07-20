824880a16c008a3f1fcd9f02cedf17120340a3f6
Your Name <you@example.com>
2025-07-20 18:36:10 +0530

    Fix responsiveness

--- apps/frontend/app/api/redeploy/route.ts
+++ apps/frontend/app/api/redeploy/route.ts
@@ -2,14 +2,14 @@ export async function POST(request: Request) {
     const TOKEN = process.env.RAILWAY_TOKEN || '';
     const ENVIRONMENT_ID = "1e09876d-cf88-4f98-9faa-7a12aa8da87a"
     const SERVICE_ID = "89ca591d-037b-42c1-8a88-00be009809c5"
-    const ACCESS_TOKEN = process.env.ACCESS_TOKEN || "";
+    const ACCESS_TOKEN = process.env.RAILWAY_ACCESS_TOKEN || "";
 
     const resp = await fetch('https://backboard.railway.com/graphql/v2', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Project-Access-Token': TOKEN,
-            'authorization': `Bearer ${ACCESS_TOKEN}`
+            'authorization': `${ACCESS_TOKEN}`
         },
         body: JSON.stringify({
             query: `
